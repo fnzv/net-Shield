f1304d93f8c0bf2e7f55cd495f7f0f217ced79bc
Sami <fnzv@users.noreply.github.com>
2017-10-18 20:56:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,5 +30,5 @@ echo "Copying example configuration... \n"
 wget -O /etc/nshield/nshield.conf https://raw.githubusercontent.com/fnzv/nShield/master/example/nshield.conf
 wget -O /etc/nshield/whitelist https://raw.githubusercontent.com/fnzv/nShield/master/example/whitelist
 
-print "Running nShield update every 1 hour.. \n"
+echo "Running nShield update every 1 hour.. \n"
 echo "30 * * * * python /home/nShield/nshield-main.py" >> /etc/crontab
