14dceb17d27a4b49e566471cb24d99dae0ebd63d
Sami <fnzv@users.noreply.github.com>
2018-01-07 21:41:47 +0100

    Update install.sh
    Go refactoring - install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-echo "Downloading & Installing Python.. \n"
+echo "Downloading & Installing Golang.. \n"
 
-apt-get install -y python
+# Golang quick install
+
+ apt-get update
+ wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
+ sudo tar -xvf go1.9.2.linux-amd64.tar.gz
+ sudo mv go /usr/local
+ echo "export GOROOT=/usr/local/go" >> /root/.bashrc
+ echo "export GOPATH=$HOME/Projects" >> /root/.bashrc
+ echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> /root/.bashrc
+ echo "Checking Golang version\n"
+ ln -s /usr/local/go/bin/go /usr/bin/go
+
 
 mkdir /etc/nshield
 
-# Configures ipt connection logging to separate file
-echo ':msg, contains, "nShield"       /var/log/nshield.log' >> /etc/rsyslog.conf && service rsyslog restart
+apt-get install -y jq software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install python-certbot-nginx  -y
 
-echo "/var/log/nshield.log {
+
+echo 'Setting ipt new log file..'
+# Configures ipt connection logging to separate file
+echo '# Log kernel generated iptables log messages to file
+:msg,contains,"nShield" /var/log/iptables.log
+& ~' >> /etc/rsyslog.d/10-ipt.conf && service rsyslog restart
+
+echo "/var/log/iptables.log {
     maxsize 100M
     hourly
     missingok
@@ -28,7 +48,3 @@ apt install -y nginx
 echo "Copying example configuration... \n"
 
 wget -O /etc/nshield/nshield.conf https://raw.githubusercontent.com/fnzv/nShield/master/example/nshield.conf
-wget -O /etc/nshield/whitelist https://raw.githubusercontent.com/fnzv/nShield/master/example/whitelist
-
-echo "Running nShield update every 1 hour.. "
-echo "30 * * * * python /home/net-Shield/nshield-main.py" >> /etc/crontab
